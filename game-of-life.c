fadc30950662c63c3890b5272266ab04905b16c7
DeathScytheZ <digital1808ta@gmail.com>
2025-01-25 16:05:47 +0100

    .

--- game-of-life.c
+++ game-of-life.c
@@ -6,14 +6,16 @@
 #define ROWS 100
 #define COLS 120 
 
-typedef enum GameScreen{START, MAIN} GameScreen;
+typedef enum Window{START, MAIN} Window;
 
 typedef struct Cell{
     bool active;
     Rectangle bounds;
 }Cell;
 
-bool underpopulated(Cell grid[ROWS][COLS], int row, int col){
+
+bool* evaluate(Cell grid[ROWS][COLS], int row, int col){
+    bool* result = malloc(3 * sizeof(bool));
     int count = 0; 
     for(int i = -1; i <= 1; i++){
         for(int j = -1; j <= 1; j++){
@@ -25,36 +27,10 @@ bool underpopulated(Cell grid[ROWS][COLS], int row, int col){
             }
         }    
     } 
-    return count < 2;
-}
-
-bool overpopulated(Cell grid[ROWS][COLS], int row, int col){
-    int count = 0;
-    for(int i = -1; i <= 1; i++){
-        for(int j = -1; j <= 1; j++){
-            if(i == 0 && j == 0) continue; 
-            if(row + i >= 0 && row + i < ROWS && col + j >= 0 && col + j < COLS){
-                if(grid[row + i][col + j].active){
-                    count ++;
-                }
-            }
-        }
-    }
-    return count > 3;
-}
-
-bool reproduce(Cell grid[ROWS][COLS], int row, int col){
-    int count = 0;
-    for(int i = -1; i <= 1; i++){
-        for(int j = -1; j <= 1; j++){
-            if(i == 0 && j == 0) continue;
-            if(row + i  >= 0 && row + i < ROWS && col + j >= 0 && col + j < COLS){
-                if(grid[row + i][col + j].active)
-                    count++;
-            }
-        }
-    }
-    return count == 3;
+    result[0] = count < 2;
+    result[1] = count > 3;
+    result[2] = count == 3;
+    return result;
 }
 
 int main(){
@@ -62,7 +38,7 @@ int main(){
     InitWindow(0, 0,"grid");
     SetTargetFPS(60);
 
-    GameScreen screen = START;
+    Window window = START;
     const float screenWidth = GetScreenWidth();
     const float screenHeight= GetScreenHeight();
     const float cellWidth = screenWidth / COLS;
@@ -90,24 +66,35 @@ int main(){
         ClearBackground(RAYWHITE);
         BeginDrawing();
         fpsCounter++;
-        switch(screen){
+        if(IsKeyPressed('R')){
+            reset = true;
+            window = START;
+        }
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            int posX = GetMouseX() / cellWidth;
+            int posY = GetMouseY() / cellHeight;
+            if(posX >= 0 && posX <  COLS && posY >= 0 && posY < ROWS){
+                grid[posY][posX].active = !grid[posY][posX].active;
+            }
+        }
+        if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
+            int posX = GetMouseX() / cellWidth;
+            int posY = GetMouseY() / cellHeight;
+            if(posX >= 0 && posX <  COLS && posY >= 0 && posY < ROWS){
+                grid[posY][posX].active = !grid[posY][posX].active;
+            }
+        }
+        if(IsKeyPressed(KEY_SPACE)){
+            pause = true;
+        }
+        if(pause){
+            window = START;
+            pause = false;
+        }
+        switch(window){
             case START:
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                    int posX = GetMouseX() / cellWidth;
-                    int posY = GetMouseY() / cellHeight;
-                    if(posX >= 0 && posX <  COLS && posY >= 0 && posY < ROWS){
-                        grid[posY][posX].active = !grid[posY][posX].active;
-                    }
-                }
-                if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
-                    int posX = GetMouseX() / cellWidth;
-                    int posY = GetMouseY() / cellHeight;
-                    if(posX >= 0 && posX <  COLS && posY >= 0 && posY < ROWS){
-                        grid[posY][posX].active = !grid[posY][posX].active;
-                    }
-                }
                 if(IsKeyPressed(KEY_ENTER)){
-                    screen = MAIN;
+                    window = MAIN;
                 }
                 break;
             case MAIN:
@@ -116,10 +103,10 @@ int main(){
                         for(int col = 0; col < COLS; col++){
                             nextGrid[row][col] = grid[row][col];
                             if(grid[row][col].active){
-                                if(underpopulated(grid, row, col) || overpopulated(grid, row, col))
+                                if(evaluate(grid, row, col)[0] || evaluate(grid, row, col)[1])
                                     nextGrid[row][col].active = false;
                             }else{
-                                if(reproduce(grid, row, col))
+                                if(evaluate(grid, row, col)[2])
                                     nextGrid[row][col].active = true;
                             }
                         }
@@ -127,29 +114,18 @@ int main(){
                     memcpy(grid, nextGrid, sizeof(grid));
                     fpsCounter = 0;
                 }
-                if(IsKeyPressed(KEY_SPACE)){
-                    pause = true;
-                }
                 break;
-        }
-        if(IsKeyPressed('R')){
-            reset = true;
-            screen = START;
         }
         for(int row = 0; row < ROWS; row++){
             for(int col = 0; col < COLS; col++){
                 if(reset)
                     grid[row][col].active = false;
-                Color cellColor = grid[row][col].active ? YELLOW: GRAY;
+                Color cellColor = grid[row][col].active ? BLACK: BEIGE;
                 DrawRectangleRec(grid[row][col].bounds, cellColor);
                 DrawRectangleLinesEx(grid[row][col].bounds, 0.3f, BLACK);
             }
         }
         reset = false;
-        if(pause){
-            screen = START;
-            pause = false;
-        }
         EndDrawing();
     }
     CloseWindow();
